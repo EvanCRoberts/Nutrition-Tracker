04c95ee5c2f98f53ef92dc228baf3dc0c43ebe41
EvanCRoberts <79759408+EvanCRoberts@users.noreply.github.com>
2021-04-06 13:45:30 -0700

    Add files via upload

---
+++ Bulking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NutritionApp
+{
+    class Bulking
+    {
+        private int age; // in whole years 
+        private double weight; //in kg 
+        private double height; // in cm
+        private string gender; // m or f 
+
+
+        //men BMR calc 
+        static void MenBMRCalc(int age, double weight, double height, string gender)
+        {
+            double  menBMR = 66.5 + 13.8 * (weight) + (height) / 6.8 * (age); 
+             
+        }
+
+        //women BMR calc 
+        static void WomenBMRCalc(int age, double weight, double height, string gender)
+        {
+            double womenBMR = 655.1 + 9.6 * (weight) + 1.9 * (height) / 4.7 * (age);
+
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NutritionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
